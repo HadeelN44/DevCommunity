fdb6409439431e5b5b9c8dd9881f656d8db5ca2c
HADEEL <hadeelalnahdi@hotmail.com>
2022-12-30 22:33:09 +0300

    Modify text color

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -15,7 +15,7 @@ class colors {
   // static Color primary = Color(0xff440A67);
 
   static Color icons = Color.fromARGB(255, 138, 135, 135);
-  static Color Text = Colors.black;
+  static Color Text = Color(0xff220533);
   static Color hyperlinks = Color.fromARGB(255, 138, 135, 135);
   static Color feedBack = Color(0xff99B7B6);
   static Color fields = Color(0xffB4AEE8).withOpacity(0.3);
